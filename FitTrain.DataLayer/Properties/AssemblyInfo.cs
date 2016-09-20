c6cafa80cc17b613d8c9a17bb37cbb3113f8f57f
ARCturus1 <arcturus1@yandex.ru>
2016-09-20 23:30:43 +0300

    Created basic structure of project

--- FitTrain.DataLayer/Properties/AssemblyInfo.cs
+++ FitTrain.DataLayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FitTrain")]
+[assembly: AssemblyTitle("FitTrain.DataLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FitTrain")]
+[assembly: AssemblyProduct("FitTrain.DataLayer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("33ca4a60-be70-4fae-9a8d-2e6b1a29d6df")]
+[assembly: Guid("7164038d-d791-46ed-bbf1-57e62db960d8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FitTrain.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FitTrain.Services.WebApiApplication" Language="C#" %>
